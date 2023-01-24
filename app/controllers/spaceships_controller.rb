57fbbbca6b3cb2722315e462c2614db87d5336b4
Yovilen Ramasamy <yovilen8@gmail.com>
2023-01-24 21:14:07 +0400

    spaceship model with controller and routes

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,2 +1,25 @@
 class SpaceshipsController < ApplicationController
+  def index
+    @spaceships = Spaceship.all
+  end
+
+  def show
+    @spaceship = Spaceship.find(params[:id])
+  end
+
+  def new
+    @spaceship = Spaceship.new
+  end
+
+  def create
+    @spaceship = Spaceship.new(spaceship_params)
+    @spaceship.save
+    redirect_to spaceship_path(@spaceship)
+  end
+
+  private
+
+  def spaceship_params
+    params.require(:spaceship).permit(:name, :banner_url, :payload, :pax, :availability, :user_id, :image_url)
+  end
 end
--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,2 +1,3 @@
 class Spaceship < ApplicationRecord
+  belongs_to :user
 end
