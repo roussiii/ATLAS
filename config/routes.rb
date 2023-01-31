f5c88f5550000ca741d15a8f0b150127c3a921e0
ROUSLAN CHIMEEA <rous0308@gmail.com>
2023-01-31 16:12:21 +0400

    added booking feature - need to review details

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :spaceships
-  resources :bookings
   resources :destinations
+  resources :bookings
 end
